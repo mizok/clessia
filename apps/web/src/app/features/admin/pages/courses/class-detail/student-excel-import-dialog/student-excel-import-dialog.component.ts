1fecf3e2670fe419e4bfb4839043e5c6bf93e261
Mizok <mizok.huang@neutec.com.tw>
2026-03-25 01:35:35 +0800

    feat(student-excel-import-dialog): scaffold component

---
+++ apps/web/src/app/features/admin/pages/courses/class-detail/student-excel-import-dialog/student-excel-import-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-excel-import-dialog',
+  imports: [],
+  templateUrl: './student-excel-import-dialog.component.html',
+  styleUrl: './student-excel-import-dialog.component.scss',
+})
+export class StudentExcelImportDialogComponent {
+
+}
