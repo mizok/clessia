558f55c9be6ef7774ccc5219f98e7940ead60d4e
Mizok <mizok.huang@neutec.com.tw>
2026-03-25 01:11:53 +0800

    feat(parent-import-dialog): scaffold component

---
+++ apps/web/src/app/features/admin/pages/parents/parent-import-dialog/parent-import-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-import-dialog',
+  imports: [],
+  templateUrl: './parent-import-dialog.component.html',
+  styleUrl: './parent-import-dialog.component.scss',
+})
+export class ParentImportDialogComponent {
+
+}
